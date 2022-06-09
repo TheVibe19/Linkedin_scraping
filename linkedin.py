843c38701fd2b3674806121a01c868da0bbfbe92
TheVibe1901 <vedant.kulkarni222@rediffmail.com>
2022-06-09 14:51:33 +0530

    scrape personal info and skills

--- linkedin.py
+++ linkedin.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import time
 global driver
 driver = webdriver.Chrome("C:/Users/vedan/Desktop/chromedriver.exe")
-
+ans=""
 driver.get("https://linkedin.com/uas/login")
 
 time.sleep(5)
@@ -18,8 +18,11 @@ pword.send_keys("Mohak!23")
 
 driver.find_element(By.XPATH,"//button[@type='submit']").click()
 
-profile_urls = ["https://www.linkedin.com/in/vecha-sumanth/","https://www.linkedin.com/in/vasu-goyal-b3b252153/","https://www.linkedin.com/in/shyam-rathod-424308238/"]
+profile_urls = ["https://www.linkedin.com/in/vasu-goyal-b3b252153/","https://www.linkedin.com/in/vecha-sumanth/","https://www.linkedin.com/in/chitransh-agnihotri/","https://www.linkedin.com/in/shyam-rathod-424308238/","https://www.linkedin.com/in/pranjali-gupta-36b12b1b4/","https://www.linkedin.com/in/vinamra-bansal-iitbhu/","https://www.linkedin.com/in/sohamagarwal85/"]
 for profile_url in profile_urls:  
+    
+    #personalinfo
+    
     driver.get(profile_url)
 
     start = time.time()
@@ -50,15 +53,55 @@ for profile_url in profile_urls:
     works_at = works_at_loc.get_text().strip()
     location_loc = intro.find_all("span", {'class': 'text-body-small'})
     location = location_loc[1].get_text().strip()
-    str1="Name -->"+name
+    str1="\nName -->"+name
     str2="\nWorks At -->"+works_at
     str3="\nLocation -->"+location
-    file1=open("data.txt","a")
-    L=str1+str2+str3
-    file1.write(L)
-    file1.close()
+    L=str1+str2+str3+"\n Skills \n"
+    
+    
+##SKILLS
+    
+    driver.get(profile_url+"details/skills/")
 
-    print("Name -->", name,
-        "\nWorks At -->", works_at,
-        "\nLocation -->", location)
+    start = time.time()
+
+    initialScroll = 0
+    finalScroll = 1000
+
+    while True:
+        driver.execute_script(f"window.scrollTo({initialScroll},{finalScroll})")
+        initialScroll=finalScroll
+        finalScroll+=1000
+        time.sleep(5)
+        end=time.time()
+
+        if round(end-start)>20:
+            break
+
+
+    src = driver.page_source
+    
+    soup = BeautifulSoup(src,  "html.parser")
+
+    
+
+    arr=[]
+    location_loc = soup.find_all("span", {'class': 'mr1 t-bold'})
+    
+    for i in location_loc:
+        skl=i.find("span",{'aria-hidden':'true'})
+        arr.append(skl.get_text().strip())
+    
+    arr=list(set(arr))   
+    
+    for i in arr:
+        L+="\n"+i
+    ans+="\n"+L    
+    
+file1=open("data.txt","a")
+   # L=str3
+file1.write(ans)
+file1.close()    
+
+
 
